a9109fe4b0838be2a39d1b8cd027e7b42c5bc916
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-09 11:15:55 +0530

    Started working on collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return (
-    <div>Collection</div>
-  )
-}
+  const { products } = useContext(ShopContext);
 
-export default Collection
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
+
+      {/* Left side filter options */}
+
+
+      {/* Right side collection */}
+    </div>
+  );
+};
+
+export default Collection;
