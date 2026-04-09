ce738f5857b2692c49dd06ecd9cba899d15073fb
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-09 17:21:36 +0530

    Completed Filter products by categories and subCategory

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -8,10 +8,46 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+  const toggleCategory = (e) => {
+    if (category.includes(e.target.value)) {
+      setCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const toggleSubCategory = (e) => {
+    if (subCategory.includes(e.target.value)) {
+      setSubCategory((prev) => prev.filter((item) => item !== e.target.value));
+    } else {
+      setSubCategory((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const applyFilter = () => {
+    let productsCopy = products.slice();
+
+    if (category.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        category.includes(item.category),
+      );
+    }
+
+    if (subCategory.length > 0) {
+      productsCopy = productsCopy.filter((item) =>
+        subCategory.includes(item.subCategory),
+      );
+    }
+
+    setFilterProducts(productsCopy);
+  };
 
   useEffect(() => {
-    setFilterProducts(products);
-  }, []);
+    applyFilter();
+  }, [category, subCategory]);
 
   return (
     <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
@@ -35,13 +71,20 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">CATEGORIES</p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input type="checkbox" value={"Men"} /> Men
+              <input type="checkbox" value={"Men"} onChange={toggleCategory} />{" "}
+              Men
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" value={"Women"} /> Women
+              <input
+                type="checkbox"
+                value={"Women"}
+                onChange={toggleCategory}
+              />{" "}
+              Women
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" value={"Kids"} /> Kids
+              <input type="checkbox" value={"Kids"} onChange={toggleCategory} />{" "}
+              Kids
             </p>
           </div>
         </div>
@@ -53,13 +96,28 @@ const Collection = () => {
           <p className="mb-3 text-sm font-medium">TYPE </p>
           <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
             <p className="flex gap-2">
-              <input type="checkbox" value={"Topwear"} /> Topwear
+              <input
+                type="checkbox"
+                value={"Topwear"}
+                onChange={toggleSubCategory}
+              />{" "}
+              Topwear
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" value={"Bottomwear"} /> Bottomwear
+              <input
+                type="checkbox"
+                value={"Bottomwear"}
+                onChange={toggleSubCategory}
+              />{" "}
+              Bottomwear
             </p>
             <p className="flex gap-2">
-              <input type="checkbox" value={"Winterwear"} /> Winterwear
+              <input
+                type="checkbox"
+                value={"Winterwear"}
+                onChange={toggleSubCategory}
+              />{" "}
+              Winterwear
             </p>
           </div>
         </div>
