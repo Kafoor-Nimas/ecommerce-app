88b331a8e2cfea596cc91f4e2e5d2a76f8f96b0e
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-12 08:19:32 +0530

    Succefully created product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -117,6 +118,12 @@ const Product = () => {
           </p>
         </div>
       </div>
+
+      {/* display related products*/}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
