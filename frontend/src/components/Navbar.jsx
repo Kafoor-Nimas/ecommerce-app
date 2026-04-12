93a31df86e251cf17529ada4fbe84b33dab1e490
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-12 10:29:57 +0530

    created getCartCount functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
-  const { setShowSearch } = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   const handleSearch = () => {
     setShowSearch(true);
@@ -67,7 +67,7 @@ const Navbar = () => {
         <Link to={"/cart"} className="relative">
           <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
           <p className="absolute -right-1.25 -bottom-1.25 w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            10
+            {getCartCount()}
           </p>
         </Link>
 
