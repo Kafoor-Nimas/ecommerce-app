7aebfa034646972f3239d416a551de539d598725
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-13 16:24:54 +0530

    Completed Login and Sign Up form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4  text-gray-800">
+    <form
+      onSubmit={onSubmitHandler}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4  text-gray-800"
+    >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
@@ -31,6 +38,27 @@ const Login = () => {
         placeholder="Password"
         required
       />
+      <div className="w-full flex justify-between text-sm mt-[-8px]">
+        <p className="cursor-pointer">Forget your password?</p>
+        {currentState === "Login" ? (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Sign Up")}
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            className="cursor-pointer"
+            onClick={() => setCurrentState("Login")}
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
     </form>
   );
 };
