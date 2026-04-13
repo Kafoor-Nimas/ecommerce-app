c71cbb13cfe9ac2df6693b805a014006f5f5816b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-13 08:22:45 +0530

    created function updateQuantity to remove from cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -50,6 +50,12 @@ const ShopContextProvider = (props) => {
     return totalCount;
   };
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
+
   const value = {
     products,
     currency,
@@ -61,6 +67,7 @@ const ShopContextProvider = (props) => {
     cartItems,
     addToCart,
     getCartCount,
+    updateQuantity,
   };
 
   return (
--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
-  const { products, currency, cartItems } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity } =
+    useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -22,7 +25,61 @@ const Cart = () => {
     }
     setCartData(tempData);
   }, [cartItems]);
-  return <div>Cart</div>;
+  return (
+    <div className="border-t border-gray-300 pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const productData = products.find(
+            (product) => product._id === item._id,
+          );
+
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-gray-300 border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  src={productData.image[0]}
+                  alt=""
+                  className="w-16 sm:w-20"
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {productData.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {productData.price}
+                    </p>
+                    <p className="px-2 sm:px-3 sm:py-1 border border-gray-300 bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                className="border border-gray-300 max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                type="number"
+                defaultValue={item.quantity}
+                min={1}
+              />
+              <img
+                onClick={() => updateQuantity(item._id, item.size, 0)}
+                src={assets.bin_icon}
+                alt=""
+                className="w-4 mr-4 sm:w-5 cursor-pointer"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
