d8bd1aa78df9cf9d4b8ece71c36afb17c04d7fbb
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-15 16:09:35 +0530

    Completed Sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr className="text-gray-300" />
+        <div className="flex w-full">
+          <Sidebar />
+        </div>
+      </>
+    </div>
   );
 };
 
