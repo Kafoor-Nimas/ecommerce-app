5d6c35e3b08e9cc69179c201c61764234b261822
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-18 16:01:27 +0530

    Created the functionality for remove product

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { backendUrl, currency } from "../App";
 import { toast } from "react-toastify";
 
-const List = () => {
+const List = ({ token }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
@@ -12,6 +12,26 @@ const List = () => {
 
       if (response.data.success) {
         setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/product/remove",
+        { id },
+        { headers: { token } },
+      );
+
+      if (response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
       } else {
         toast.error(response.data.message);
       }
@@ -51,7 +71,7 @@ const List = () => {
               {currency}
               {item.price}
             </p>
-            <p className="text-right md:text-center cursor-pointer text-lg">
+            <p onClick={()=>removeProduct(item._id)} className="text-right md:text-center cursor-pointer text-lg">
               X
             </p>
           </div>
