ad58bfa4cb3db7832e7f7a023a854cfbb881525c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-19 09:33:26 +0530

    Add token retrieval from localStorage in ShopContextProvider

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -97,6 +97,12 @@ const ShopContextProvider = (props) => {
     getProductsData();
   }, []);
 
+  useEffect(() => {
+    if (!token && localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
+
   const value = {
     products,
     currency,
