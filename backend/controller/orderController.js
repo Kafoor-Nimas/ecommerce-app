036e813065fb564ae3333df7ae3aecca4ec96b6f
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-04-20 12:19:33 +0530

    Placing order using COD Method API

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,5 +1,32 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel";
+
 // Placing order using COD Method
-const placeOrder = async (req, res) => {};
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    // Whenever an order is placed, the cart will be cleared
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Placing order using Stripe Method
 const placeOrderStripe = async (req, res) => {};
